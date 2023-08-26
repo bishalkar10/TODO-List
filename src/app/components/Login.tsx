3a72076b0f6d65963813ae84dc0aa0158ca33766
Bishal Kar <bishalkar10@gmail.com>
2023-08-26 22:37:02 +0530

    New component

---
+++ src/app/components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../styles/page.module.scss";
+import Image from 'next/image'
+
+export default function Login() {
+  return (
+    <div className={styles.login}>
+      <Image className={styles.image} src="" alt="" />
+    </div>
+  )
+}
