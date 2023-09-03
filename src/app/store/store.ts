f23772adaf30c05f65abaab73f049d27e8d9503b
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-09-03 09:32:08 +0000

    deleted the toggleshow state logic and removed the file too

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import showButtonsSlice from "./reducers/toggleShow";
 import tasksSlice from "./reducers/tasks";
 
 const store = configureStore({
   reducer: {
-    showButtons: showButtonsSlice.reducer,
     tasks: tasksSlice.reducer,
   },
 });
