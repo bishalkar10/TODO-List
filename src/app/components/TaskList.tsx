ade313e2dedbf7a00eba8fd6ec33c9488da401e4
Bishal Kar <bishalkar10@gmail.com>
2023-09-03 21:16:09 +0530

    Replaced TaskStates with RootState as it was causing typescript errors

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -1,11 +1,11 @@
 import styles from "../styles/page.module.scss";
 import Task from "./Task";
-import {TasksState} from "../store/store";
-import {useSelector} from "react-redux";
+import { RootState } from "../store/store";
+import { useSelector } from "react-redux";
 
-export default function TaskList() { 
-  //get the task state from redux store 
-  const tasksState = useSelector((state: TasksState) => state.tasks.values);
+export default function TaskList() {
+  //get the root state from redux store 
+  const tasksState = useSelector((state: RootState) => state.taskList.tasks);
   //taskList array to render all the 'Task' component
   const taskList = Array.from(tasksState.values()).map((item) => (
     <Task
