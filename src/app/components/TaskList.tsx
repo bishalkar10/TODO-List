2303a3e1a0edbfca961c6cf3ae58ef735afaef5b
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-09-03 13:06:26 +0000

    replaced local dataObject with Redux state

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -1,57 +1,13 @@
 import styles from "../styles/page.module.scss";
 import Task from "./Task";
+import {TasksState} from "../store/store";
+import {useSelector} from "react-redux";
 
-interface DataItem {
-  id: string;
-  title: string;
-  description: string;
-}
-
-const dataObject: Map<string, DataItem> = new Map([
-  [
-    "ab1",
-    {
-      id: "ab1",
-      title: "Task Title 1",
-      description: "Lorem ipsum dolor...",
-    },
-  ],
-  [
-    "ab2",
-    {
-      id: "ab2",
-      title: "Task Title 2",
-      description: "Lorem ipsum dolor...",
-    },
-  ],
-  [
-    "x@1",
-    {
-      id: "x@1",
-      title: "Task Title 3",
-      description: "Lorem ipsum dolor...",
-    },
-  ],
-  [
-    "xy2",
-    {
-      id: "xy2",
-      title: "Task Title 4",
-      description: "Lorem ipsum dolor...",
-    },
-  ],
-  [
-    "a2y",
-    {
-      id: "a2y",
-      title: "Task Title 5",
-      description: "Lorem ipsum dolor...",
-    },
-  ],
-]);
-
-export default function TaskList() {
-  const taskList = Array.from(dataObject.values()).map((item) => (
+export default function TaskList() { 
+  //get the task state from redux store 
+  const tasksState = useSelector((state: TasksState) => state.tasks.values);
+  //taskList array to render all the 'Task' component
+  const taskList = Array.from(tasksState.values()).map((item) => (
     <Task
       key={item.id}
       id={item.id}
