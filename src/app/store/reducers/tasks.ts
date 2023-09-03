31a7dd07b49481e429d88669d3824fd73675f430
Bishal Kar <bishalkar10@gmail.com>
2023-09-03 22:25:59 +0530

    Now using Arrays instead of Map

--- src/app/store/reducers/tasks.ts
+++ src/app/store/reducers/tasks.ts
@@ -7,52 +7,37 @@ export type Task = {
 };
 
 export type TasksState = {
-  tasks: Map<string, Task>;
+  tasks: Task[];
 };
 
 const initialState: TasksState = {
-  tasks: new Map([
-    [
-      "ab1",
-      {
-        id: "ab1",
-        title: "Task Title 1",
-        description: "Lorem ipsum dolor...",
-      },
-    ],
-    [
-      "ab2",
-      {
-        id: "ab2",
-        title: "Task Title 2",
-        description: "Lorem ipsum dolor...",
-      },
-    ],
-    [
-      "x@1",
-      {
-        id: "x@1",
-        title: "Task Title 3",
-        description: "Lorem ipsum dolor...",
-      },
-    ],
-    [
-      "xy2",
-      {
-        id: "xy2",
-        title: "Task Title 4",
-        description: "Lorem ipsum dolor...",
-      },
-    ],
-    [
-      "a2y",
-      {
-        id: "a2y",
-        title: "Task Title 5",
-        description: "Lorem ipsum dolor...",
-      },
-    ],
-  ]),
+  tasks: [
+    {
+      id: "ab1",
+      title: "Task Title 1",
+      description: "Lorem ipsum dolor...",
+    },
+    {
+      id: "ab2",
+      title: "Task Title 2",
+      description: "Lorem ipsum dolor...",
+    },
+    {
+      id: "x@1",
+      title: "Task Title 3",
+      description: "Lorem ipsum dolor...",
+    },
+    {
+      id: "xy2",
+      title: "Task Title 4",
+      description: "Lorem ipsum dolor...",
+    },
+    {
+      id: "a2y",
+      title: "Task Title 5",
+      description: "Lorem ipsum dolor...",
+    },
+  ],
 };
 
 const tasksSlice = createSlice({
@@ -60,15 +45,25 @@ const tasksSlice = createSlice({
   initialState,
   reducers: {
     addTask: (state, action) => {
-      state.tasks.set(action.payload.id, action.payload.task);
+      state.tasks.push(action.payload.task);
       return state;
     },
     deleteTask: (state, action) => {
-      state.tasks.delete(action.payload.id);
+      const taskIndex = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      if (taskIndex !== -1) {
+        state.tasks.splice(taskIndex, 1);
+      }
       return state;
     },
     updateTask: (state, action) => {
-      state.tasks.set(action.payload.id, action.payload.task);
+      const taskIndex = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      if (taskIndex !== -1) {
+        state.tasks[taskIndex] = action.payload.task;
+      }
       return state;
     },
   },
