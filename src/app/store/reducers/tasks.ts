be74e5dcdfd6b932df6eeb2096a7bf9dbaa93b5e
Bishal Kar <bishalkar10@gmail.com>
2023-09-03 21:14:53 +0530

    changed Map varible values  inside initialState to tasks to avoid typescript errors

--- src/app/store/reducers/tasks.ts
+++ src/app/store/reducers/tasks.ts
@@ -8,52 +8,52 @@ export type Task = {
 
 export type TasksState = {
   tasks: Map<string, Task>;
-}; 
+};
 
-const initialState : TasksState = {
-    values: new Map([
-      [
-        "ab1",
-        {
-          id: "ab1",
-          title: "Task Title 1",
-          description: "Lorem ipsum dolor...",
-        },
-      ],
-      [
-        "ab2",
-        {
-          id: "ab2",
-          title: "Task Title 2",
-          description: "Lorem ipsum dolor...",
-        },
-      ],
-      [
-        "x@1",
-        {
-          id: "x@1",
-          title: "Task Title 3",
-          description: "Lorem ipsum dolor...",
-        },
-      ],
-      [
-        "xy2",
-        {
-          id: "xy2",
-          title: "Task Title 4",
-          description: "Lorem ipsum dolor...",
-        },
-      ],
-      [
-        "a2y",
-        {
-          id: "a2y",
-          title: "Task Title 5",
-          description: "Lorem ipsum dolor...",
-        },
-      ],
-    ]),
-}
+const initialState: TasksState = {
+  tasks: new Map([
+    [
+      "ab1",
+      {
+        id: "ab1",
+        title: "Task Title 1",
+        description: "Lorem ipsum dolor...",
+      },
+    ],
+    [
+      "ab2",
+      {
+        id: "ab2",
+        title: "Task Title 2",
+        description: "Lorem ipsum dolor...",
+      },
+    ],
+    [
+      "x@1",
+      {
+        id: "x@1",
+        title: "Task Title 3",
+        description: "Lorem ipsum dolor...",
+      },
+    ],
+    [
+      "xy2",
+      {
+        id: "xy2",
+        title: "Task Title 4",
+        description: "Lorem ipsum dolor...",
+      },
+    ],
+    [
+      "a2y",
+      {
+        id: "a2y",
+        title: "Task Title 5",
+        description: "Lorem ipsum dolor...",
+      },
+    ],
+  ]),
+};
 
 const tasksSlice = createSlice({
   name: "tasks",
