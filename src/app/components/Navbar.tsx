24ae6bde987f699a2f741b79a167b49f7b266a85
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-09-03 13:03:18 +0000

    removed fontawesome icon and fixed the "Image" tag src attribute

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/page.module.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
     <header className={styles.header}>
       <h5 className={styles.h1}>Todo List</h5>
-      <div className={styles.userIconDiv}>
-        <FontAwesomeIcon className={styles.userIcon} icon={faUser} />
-      </div>
-      <Image className={styles.image} src="" alt="" />
+      <Image 
+      className={styles.image} 
+      src='/user.png'
+      width={56} 
+      height={56} 
+      alt="user" />
     </header>
   )
 }
