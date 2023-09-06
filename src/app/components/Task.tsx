d2c31a0465b27248f505c966949d343c3e6addbc
Bishal Kar <bishalkar10@gmail.com>
2023-09-06 16:52:26 +0530

    Checkbox introduced, implemeted the deleteTask and editTask functions.
    -Passing the props recieved by the 'Task' component to the Button Component.
    -directly updating the task state when check/uncheck the input box.

--- src/app/components/Task.tsx
+++ src/app/components/Task.tsx
@@ -4,47 +4,52 @@ import styles from "../styles/page.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch } from "react-redux";
-import { deleteTask, addTask, updateTask } from "../store/reducers/tasks";
+import { deleteTask, updateTask } from "../store/reducers/tasks";
+import { toggleModal } from "../store/reducers/modal";
+import { updateFormData } from "../store/reducers/formData";
 
 interface TaskProps {
   id: string,
   title: string,
   description: string,
+  checked: boolean,
 }
 
-export default function Task({ id, title, description }: TaskProps) {
+export default function Task(props: TaskProps) {
+  const { id, title, description, checked } = props;
   const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
+
+  function handleChange() {
+    dispatch(updateTask({ id, title, description, checked: !checked }));
+  }
+
   return (
     <div
       data-tasks-id={id}
       className={styles.taskContainer}
       onMouseEnter={() => setShow(true)}
       onMouseLeave={() => setShow(false)}
-    >
+    > <input type="checkbox" checked={checked} onChange={handleChange} />
       <div className={styles.task}>
         <p className={styles.taskTitle}>{title}</p>
         <p className={styles.taskDesc}>{description}</p>
       </div>
-      {/* We are not  */}
-      <Buttons show={show} />
+      <Buttons show={show} {...props} />
     </div>
   );
 }
 
-export function Buttons({ show }: { show: boolean }) {
-  const dispatch = useDispatch()
+export function Buttons({ show, id, title, description, checked }: TaskProps & { show: boolean }) {
+  const dispatch = useDispatch();
 
-  function deleteTaskHandler(event: any) {
-    const targetElement = event.currentTarget.parentElement?.parentElement;
-    console.log("clicked delete", targetElement);
-    if (targetElement) {
-      const ID = targetElement.getAttribute("data-tasks-id");
-      dispatch(deleteTask({ id: ID }));
-    }
+  function deleteTaskHandler() {
+    dispatch(deleteTask({ id }));
   }
 
-  function editTask(event: any) {
-    const targetElement = event.currentTarget.parentElement?.parentElement
+  function editTask() {
+    dispatch(updateFormData({ id, title, description, checked }))
+    dispatch(toggleModal({ mode: "update" }))
   }
 
   return (
