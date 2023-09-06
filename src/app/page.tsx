737f810f612e03cd19ec5394c1e069b0fd5c4221
Bishal Kar <bishalkar10@gmail.com>
2023-09-06 16:44:20 +0530

    Added 'Modal' component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import store from './store/store';
 import Navbar from "./components/Navbar";
 import Add from "./components/Add";
 import TaskList from "./components/TaskList";
+import Modal from "./components/Modal";
 
 export default function Home() {
   return (
     <Provider store={store}>
-
       <Navbar />
       <TaskList />
       <Add />
+      <Modal />
 
     </Provider>
   )
