36601854ba3ce35f9483117783b73461d983cfa2
Bishal Kar <bishalkar10@gmail.com>
2023-09-06 16:36:31 +0530

    The add button is now functional.
    - We aren't using 'button' tag cause the '+' inside the button element will not be centered.
    - When clicks the 'div' then open the modal in 'add' mode.
    - When the show state is true then doesn't accept clicks.

--- src/app/components/Add.tsx
+++ src/app/components/Add.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import styles from "../styles/page.module.scss";
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleModal } from '../store/reducers/modal';
+import { RootState } from '../store/store';
+
 export default function Add() {
+  const show = useSelector((state: RootState) => state.modal.isOpen);
+  const dispatch = useDispatch();
+  function openModal() {
+    // if show is true then return . It is meant to disable the button when modal is open
+    if (show) return;
+    dispatch(toggleModal({ mode: 'add' }))
+  }
+  // we are not usign 'button' tag as usign it will cause the '+' icon shift to the bottom and not centered. Increased font-size causes this issue
   return (
-    <div className={styles.addBtn}>+</div>
+    <div
+      className={styles.addBtn}
+      onClick={openModal}
+    >+</div>
   )
 }
