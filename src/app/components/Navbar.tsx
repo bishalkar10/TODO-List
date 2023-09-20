4a0aee0a2ccc57477ce5eaabe3a85ae22b7a98a5
Bishal Kar <bishalkar10@gmail.com>
2023-09-20 12:29:50 +0530

    Refactored Navbar
    - Importing the redux states
    - Now Image will change accordingly to when signInStatus state

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/page.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
 
 export default function Navbar() {
+  const user = useSelector((state: RootState) => state.user)
+  const signInStatus = useSelector((state: RootState) => state.user.signedIn)
+
   return (
     <header className={styles.header}>
       <h5 className={styles.h1}>Todo List</h5>
-      <Image 
-      className={styles.image} 
-      src='/user.png'
-      width={56} 
-      height={56} 
-      alt="user" />
+      <Image
+        className={styles.image}
+        src={signInStatus ? user.photo : '/user.png'} // if signed in then render the user photo from Gmail else render the default user.png 
+        width={56}
+        height={56}
+        alt="user" />
     </header>
   )
 }
