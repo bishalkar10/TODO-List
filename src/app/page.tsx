609441e2c29cf5532f45c69ba873fdb6a8d27054
Bishal Kar <bishalkar10@gmail.com>
2023-09-20 12:31:21 +0530

    implemented Login component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import Navbar from "./components/Navbar";
 import Add from "./components/Add";
 import TaskList from "./components/TaskList";
 import Modal from "./components/Modal";
+import Login from "./components/Login";
 
 export default function Home() {
   return (
     <Provider store={store}>
       <Navbar />
+      <Login />
       <TaskList />
       <Add />
       <Modal />
