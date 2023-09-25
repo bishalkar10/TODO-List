fe3027cd9b9b23311f7a68d033f87c35b94e3ea7
Bishal Kar <bishalkar10@gmail.com>
2023-09-25 21:59:59 +0530

    Refactor userSlice state and updateUser reducer
    In this commit:
    
    - Refactored the userSlice state to include a 'uid' field.
    - Updated the 'updateUser' reducer to handle the 'uid' field in addition to 'name', 'photo', and 'email'.

--- src/app/store/reducers/user.ts
+++ src/app/store/reducers/user.ts
@@ -4,6 +4,7 @@ const userSlice = createSlice({
   name: "user",
   initialState: {
     signedIn: false,
+    uid: "",
     name: "",
     photo: "",
     email: "",
@@ -13,7 +14,8 @@ const userSlice = createSlice({
       state.signedIn = action.payload;
     },
     updateUser: (state, action) => {
-      const { name, photo, email } = action.payload;
+      const { uid, name, photo, email } = action.payload;
+      state.uid = uid;
       state.name = name;
       state.photo = photo;
       state.email = email;
