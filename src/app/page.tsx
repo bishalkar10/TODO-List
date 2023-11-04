fc1ddf868d677d3dfe302e236d740753e90d107e
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-11-04 12:19:31 +0000

    refactored code
    - created 'App' component and put all the component in it.
    Reason : We need to use the 'isOpen' state and the component has to be wrapped in the 'Provider'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
-"use client"
+"use client";
 import { Provider } from "react-redux";
-import store from './store/store';
+import store from "./store/store";
 import Navbar from "./components/Navbar";
 import Add from "./components/Add";
 import TaskList from "./components/TaskList";
 import Modal from "./components/Modal";
 import Login from "./components/Login";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./store/store";
 
-export default function Home() {
+export function App() {
+  const isOpen = useSelector((state: RootState) => state.popup.isOpen);
+
   return (
-    <Provider store={store}>
+    <div>
       <Navbar />
-      <Login />
+      {isOpen ? <Login /> : null}
       <TaskList />
       <Add />
       <Modal />
+    </div>
+  );
+}
 
+// Wrap the entire component tree with the Provider
+export default function Home() {
+  return (
+    <Provider store={store}>
+      <App />
     </Provider>
-  )
+  );
 }
