3397139b5bcd0b2b7f9f4d60cf75cecb76099ab6
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-11-04 11:34:40 +0000

    Refactored code
    - Added handleclick function to the image icon. clicking on the image will toggle the popup

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from '../styles/page.module.scss';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/store';
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/page.module.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../store/store";
+import { togglePopup, closePopup } from "../store/reducers/popup";
 
 export default function Navbar() {
-  const user = useSelector((state: RootState) => state.user)
-  const signInStatus = useSelector((state: RootState) => state.user.signedIn)
+  const user = useSelector((state: RootState) => state.user);
+  const signInStatus = useSelector((state: RootState) => state.user.signedIn);
+  const dispatch = useDispatch();
+  const isOpen = useSelector((state: RootState) => state.popup.isOpen);
+
+  function handleClick() {
+    dispatch(togglePopup());
+  }
 
   return (
     <header className={styles.header}>
       <h5 className={styles.h1}>Todo List</h5>
       <Image
+        onClick={handleClick}
         className={styles.image}
-        src={signInStatus ? user.photo : '/user.png'} // if signed in then render the user photo from Gmail else render the default user.png 
+        src={signInStatus ? user.photo : "/user.png"} // if signed in then render the user photo from Gmail else render the default user.png
         width={56}
         height={56}
-        alt="user" />
+        alt="user"
+      />
     </header>
-  )
+  );
 }
